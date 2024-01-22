c34f5152b9f342c25399589af7d0376ea8fb6b08
Sebastian Reategui <seb.reategui@gmail.com>
2024-01-22 13:20:28 +1100

    weird encoding/utf8/ascii bug that be buggin' me

---
+++ test.py
@@ -0,0 +1,38 @@
+print("Begin: ✅ Emoji on stdout")
+print('---')
+
+print("Try to load simple file as string")
+stream = open('./data.txt', 'r')
+print('    data.txt:', stream.read())
+print('---')
+
+print('Try to load YAML file with emojis')
+import yaml
+stream = open('./data.yml', 'r')
+print( yaml.safe_load(stream) )
+print('---')
+
+print('Try to import pydavinci')
+import pydavinci
+print('Imported pydavinci. Version:', pydavinci.__version__)
+print("✅ Emoji after pydavinci import")
+print('---')
+
+print('Try to create a Resolve object/contact API')
+Resolve = pydavinci.davinci.Resolve()
+print(Resolve)
+print("✅ Emoji after printing Resolve obj")
+print('---')
+
+print("Try to load simple file as string")
+stream = open('./data.txt', 'r')
+print('data.txt:', stream.read())
+print("✅ Emoji after reading back a file")
+print('---')
+
+print('Try to load emojis again...')
+stream = open('./data.yml', 'r')
+print( yaml.safe_load(stream) )
+print("✅ Emoji after safe loading yaml file")
+print('---')
+
